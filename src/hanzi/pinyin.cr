d9492226b4d8583eb560f314fc79f5ea86c9eaf2
Nipinium <nipinium@gmail.com>
2019-02-07 22:45:09 +0700

    Run formatter

--- src/hanzi/pinyin.cr
+++ src/hanzi/pinyin.cr
@@ -4,11 +4,11 @@ module Hanzi
   module Pinyin
     def self.number_to_tone(pinyin)
       pinyin
-      .downcase
-      .gsub("u:", "ü")
-      .split(/[\s\-]/x)
-      .map{ |word| PINYIN_TONES.fetch(word, word) }
-      .join(" ")
+        .downcase
+        .gsub("u:", "ü")
+        .split(/[\s\-]/x)
+        .map { |word| PINYIN_TONES.fetch(word, word) }
+        .join(" ")
     end
   end
 end
