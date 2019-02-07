16fe1460718a16c5b43dbb9df5be43d7f37e02ca
Nipinium <nipinium@gmail.com>
2019-02-07 16:16:11 +0700

    Add more edge cases for pinyin convert

--- spec/hanzi_spec.cr
+++ spec/hanzi_spec.cr
@@ -1,7 +1,19 @@
 require "./spec_helper"
 
 describe Hanzi do
-  it "correct convert pinyin numbers tones" do
+  it "correctly convert pinyin numbers tones" do
     Hanzi::Pinyin.number_to_tone("pin1 yin1").should eq("pīn yīn")
+  end
+
+  it "works with difference separator" do
+    Hanzi::Pinyin.number_to_tone("pin1-yin1").should eq("pīn yīn")
+  end
+
+  it "correctly handle non-existing works" do
+    Hanzi::Pinyin.number_to_tone("ABC XYZ").should eq("abc xyz")
+  end
+
+  it "works with ü vowel" do
+    Hanzi::Pinyin.number_to_tone("lu:1").should eq("lǖ")
   end
 end
--- src/hanzi/pinyin.cr
+++ src/hanzi/pinyin.cr
@@ -1,10 +1,15 @@
 module Hanzi
-  FILE = File.join __DIR__, "../../priv/tones.txt"
-  TONES = File.read_lines(FILE).map{|line| line.split("=", 2)}.to_h
+  INPUT = File.join __DIR__, "../../priv/tones.txt"
+  TONES = File.read_lines(INPUT).map{|line| line.split("=", 2)}.to_h
 
   module Pinyin
     def self.number_to_tone(pinyin)
-      pinyin.split(" ").map{|word| TONES[word]? || word}.join(" ")
+      pinyin
+      .downcase
+      .gsub("u:", "ü")
+      .split(/[\s\-]/x)
+      .map{ |word| TONES.fetch(word, word) }
+      .join(" ")
     end
   end
 end
